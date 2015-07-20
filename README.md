72f47969a28390068392028ef1d54fe0075534fa
Nemo <zhhjchina@gmail.com>
2015-07-20 23:04:55 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mah
+# Latino
+Thinking about the project's name could be racist. [source](https://www.youtube.com/watch?v=hSzmVFF58Mo)
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp')
+  , minifyCSS = require('gulp-minify-css')
+  , uglify = require('gulp-uglify')
+  , gulpJade = require('gulp-jade')
+  , rename = require('gulp-rename')
+  , del = require('del')
+  , jade = require('jade')
+  ;
+
+var paths = {
+  watch: 'src/**/*.jade',
+  jade: ['src/**/*.jade', '!src/include/*.jade'],
+  js: 'src/js/**/*',
+  css: 'src/css/**/*',
+  bin: ['src/img*/**/*', 'src/fonts*/**/*'],
+};
+
+gulp.task('clean', function(cb) {
+  del(['build'], cb);
+});
+
+gulp.task('copy', ['clean'], function() {
+  return gulp.src(paths.bin).pipe(gulp.dest('build/'));
+});
+
+gulp.task('css', ['clean'], function() {
+  return gulp.src(paths.css)
+    .pipe(minifyCSS({ keepBreaks: false, keepSpecialComments: 0 }))
+    .pipe(gulp.dest('build/css/'))
+    ;
+});
+
+gulp.task('js', ['clean'], function() {
+  return gulp.src(paths.js)
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js/'))
+    ;
+});
+
+gulp.task('jade', ['clean'], function() {
+  return gulp.src(paths.jade)
+    .pipe(gulpJade({jade: jade}))
+    .pipe(rename({extname: '.html'}))
+    .pipe(gulp.dest('build/'))
+    ;
+});
+
+gulp.task('build', ['css', 'js', 'jade', 'copy']);
+
+
+gulp.task('dev-clean-html', function(cb) {
+  del('src/**/*.html', cb);
+});
+
+gulp.task('dev-jade', ['dev-clean-html'], function() {
+  return gulp.src(paths.jade)
+    .pipe(gulpJade({jade: jade}))
+    .pipe(rename({extname: '.html'}))
+    .pipe(gulp.dest('src/'))
+    ;
+});
+
+gulp.task('dev', ['dev-jade'], function() {
+  gulp.watch(paths.watch, ['dev-jade']);
+});
